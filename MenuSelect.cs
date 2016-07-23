eb943b274a78868a80d95322ba642b00ee98cf28
AdrianArielRodriguez <adrian361@hotmail.com>
2016-07-23 02:07:46 -0300

    Carga
    Carga del archivo para cambiar de escenas, dentro del contexto del menú, a partir de un botton

---
+++ MenuSelect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.SceneManagement; PARA LA VERSION Unity5.3 
+
+
+public class MenuSelect : MonoBehaviour {
+
+	public void CambiarEscena(int EscenaAcambiar)
+    {
+        Debug.Log("Para ver en consola");
+        Application.LoadLevel (EscenaAcambiar); //version 5.3: SceneManager.LoadScene (int)
+    }
+}
